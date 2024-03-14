1628c4a31d2e2f9f7ce0dcdcf37388c6ff02a212
Alexis Maldonado <alexisalex31@hotmail.com>
2024-03-14 18:33:46 -0500

    cambios

--- Backend/SumincogarBackend/SumincogarBackend/obj/Debug/net6.0/SumincogarBackend.AssemblyInfo.cs
+++ Backend/SumincogarBackend/SumincogarBackend/obj/Debug/net6.0/SumincogarBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SumincogarBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+287607fd4272af1c20a177b8ea3183bd776037de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6c838032ca01503d65a6ad3446f441dc0e35619")]
 [assembly: System.Reflection.AssemblyProductAttribute("SumincogarBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SumincogarBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
